801edc1d553c6170b4ce42a5d39f0144c736884f
Tolu <toluwalopeajisola@gmail.com>
2022-03-04 17:24:56 -0500

    initialize test

--- src/test/java/book/ApplicationTest.java
+++ src/test/java/book/ApplicationTest.java
@@ -1,7 +1,71 @@
 package book;
 
 import static org.junit.Assert.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.thymeleaf.spring5.expression.Mvc;
+import static org.junit.Assert.*;
+import java.nio.charset.Charset;
 
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    BuddyInfoRepository buddyRepo;
+
+    @Autowired
+    AddressBookRepository addressBookRepository;
+
+    @Test
+    public void addressBookShouldBeEmpty() throws Exception {
+        String url = "/addressbook";
+        this.mockMvc.perform(get(url))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"));
+                //.andExpect(jsonPath("_embedded.addressbook", hasSize(0)));
+
+    }
+
+    @Test
+    public void createAddressBook() throws Exception {
+        String url = "/addressbook";
+        this.mockMvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andDo(print())
+                .andExpect(status().is(200));
+
+        assertNotEquals(this.addressBookRepository.count(), 0);
+    }
+
+    @Test
+    public void createNewBuddy() throws Exception {
+        String url = "/buddy";
+        MvcResult result = this.mockMvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andDo(print())
+                .andExpect(status().is(400))
+                .andReturn();
+    }
 
 }
