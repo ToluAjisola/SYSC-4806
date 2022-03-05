614f56dc6d0497b8a0b8caa32939d6dddad0703a
Tolu <toluwalopeajisola@gmail.com>
2022-03-04 20:44:31 -0500

    heroku tests

---
+++ src/test/java/book/TestingWebApplication.java
@@ -0,0 +1,12 @@
+package book;
+
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplication {
+    @Test
+    public void contextLoads() {
+    }
+}
